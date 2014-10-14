dc8121c77fc5898d4bc9d7494f9403cda57fe719
Thomas Kjøglum <thomas@kjoglum.com>
2014-10-14 13:44:58 +0200

    Created a getTimeStamps method to read timestamps from file. And the bot now writes the timestamps to irc

--- smartBot.py
+++ smartBot.py
@@ -6,24 +6,38 @@ server = "irc.inet.tele.dk" # Server
 channel = "#A-177" # Channel
 botnick = "SmartBot" # Your bots nick
 
+kristian = "88:32:9B:62:85:F0"
+thomas = "64:A3:CB:77:1E:27"
+orjan = "34:23:BA:93:54:D5"
+
+def getTimeStamps():
+    inputFile = open('output-01.csv')
+    sendmsg("The timestamp is when they were last seen")
+    sendmsg("")
+    for line in inputFile.read().splitlines():
+        if kristian in line:
+            sendmsg("Kristian: " + line.split(',')[2])
+        elif thomas in line:
+            sendmsg("Thomas: " + line.split(',')[2])
+        elif orjan in line:
+            sendmsg("Orjan: " + line.split(',')[2])
+
 
 def ping(): # This is our first function! It will respond to server Pings.
     ircsock.send("PONG :pingis\n")
 
-def sendmsg(chan , msg): # This is the send message function, it simply sends messages to the channel.
-    ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n")
+def sendmsg(msg): # This is the send message function, it simply sends messages to the channel.
+    ircsock.send("PRIVMSG " + channel + " :" + msg + "\n")
 
 def joinchan(chan): # This function is used to join channels.
-    ircsock.send("JOIN "+ chan +"\n")
+    ircsock.send("JOIN " + chan + "\n")
 
 def hello(): # This function responds to a user that inputs "Hello Mybot"
-    ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
+    ircsock.send("PRIVMSG " + channel + " :Hello!\n")
 
 def inOffice():
-    #
-    # Lettere a bare lese fra fil nar var sist gang personene ble sett
-    #
-    ircsock.send("PRIVMSG "+ channel +" :Kristian har ikke ordnet funksjonen enda!\n")
+    getTimeStamps()
+
 
 
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
