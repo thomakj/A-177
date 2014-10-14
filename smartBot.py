a07e746070a5bf8e898224ed3e2b5b050b15f8f7
Thomas Kjøglum <thomas@kjoglum.com>
2014-10-14 12:03:02 +0200

    Another norwegian char

--- smartBot.py
+++ smartBot.py
@@ -21,7 +21,7 @@ def hello(): # This function responds to a user that inputs "Hello Mybot"
 
 def inOffice():
     #
-    # Lettere a bare lese fra fil når var sist gang personene ble sett
+    # Lettere a bare lese fra fil nar var sist gang personene ble sett
     #
     ircsock.send("PRIVMSG"+ channel +" :Kristian har ikke ordnet funksjonen enda!\n")
 
